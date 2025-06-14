3bd736551860db8d0d4a87706a69df6c3bbf97ff
Vinicius Dos Santos Girotti <vinicius0621@hotmail.com>
2025-06-14 11:06:00 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,15 +1,11 @@
 # Exemplo de conexão MySQL com variáveis de ambiente
 
 import mysql.connector
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
 
 def conectar():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
+        host("localhost"),
+        username("**********"),
+        password("**********"),
+        database("tarefas_db")
     )
